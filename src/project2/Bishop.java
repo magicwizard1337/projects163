22e33a9ef992152f400c3d626a42f594c03bf474
magciwizard1337 <nguyendanielftw@gmail.com>
2022-02-15 17:33:35 -0500

    initial

---
+++ src/project2/Bishop.java
@@ -0,0 +1,19 @@
+package project2;
+
+public class Bishop extends ChessPiece {
+
+	public Bishop(Player player) {
+		super(player);
+	}
+
+	public String type() {
+		return "Bishop";
+	}
+	
+	public boolean isValidMove(Move move, IChessPiece[][] board) {
+
+		return true;
+        // More code is needed
+		
+	}
+}
---
+++ src/project2/King.java
@@ -0,0 +1,18 @@
+package project2;
+
+public class King extends ChessPiece {
+
+	public King(Player player) {
+		super(player);
+	}
+
+	public String type() {
+		return "King";
+	}
+	
+	public boolean isValidMove(Move move, IChessPiece[][] board) {
+		boolean valid = true;
+        // More code is needed
+		return valid;
+	}
+}
